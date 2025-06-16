e738bf30cbc0b7d98a691390a9b12abb7815053f
Aashutosh Poudel <apoudel01@wm.edu>
2025-06-15 22:11:32 -0400

    add helper functions for working with aesgcm refactor old encrypt/decrypt functions
    Co-authored-by: Matthew Berthoud <mwberthoud@wm.edu>

--- pkg/mutil/util.go
+++ pkg/mutil/util.go
@@ -1,11 +1,18 @@
 package mutil
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
 	"log"
 	"sync"
 
 	"github.com/etclab/pre"
 )
+
+const KeySize = 32
+const NonceSize = 12
 
 func LogWithPrefix(prefix string) func(format string, v ...interface{}) {
 	return func(format string, v ...interface{}) {
@@ -31,4 +38,46 @@ func GSafeReadFromMap[K comparable, V PreKeys](key K, gMap map[K]*V, lock *sync.
 	lock.RLock()
 	defer lock.RUnlock()
 	return gMap[key]
+}
+
+// placeholder encryption
+func FakeEncrypt(plaintext []byte) ([]byte, error) {
+	return bytes.ToUpper(plaintext), nil
+}
+
+// placeholder decryption
+func FakeDecrypt(encBody []byte) ([]byte, error) {
+	return bytes.ToLower(encBody), nil
+}
+
+func NewAESGCM(key []byte) (cipher.AEAD, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("aes.NewCipher failed: %v", err)
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("cipher.NewGCM failed: %v", err)
+	}
+
+	return aesgcm, nil
+}
+
+func AESGCMEncrypt(key, plaintext []byte) ([]byte, error) {
+	aesgcm, err := NewAESGCM(key)
+	if err != nil {
+		return nil, err
+	}
+	nonce := make([]byte, NonceSize) // zero nonce
+	return aesgcm.Seal(plaintext[:0], nonce, plaintext, nil), nil
+}
+
+func AESGCMDecrypt(key, ciphertext []byte) ([]byte, error) {
+	aesgcm, err := NewAESGCM(key)
+	if err != nil {
+		return nil, err
+	}
+	nonce := make([]byte, NonceSize) // zero nonce
+	return aesgcm.Open(nil, nonce, ciphertext, nil)
 }
