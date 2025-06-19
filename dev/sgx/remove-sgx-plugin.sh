ccef97c73bed13ca09ef6b3c647ef400264f3dab
Aashutosh Poudel <apoudel01@wm.edu>
2025-06-18 23:30:06 -0400

    update verson

--- dev/sgx/remove-sgx-plugin.sh
+++ dev/sgx/remove-sgx-plugin.sh
@@ -3,7 +3,7 @@
 # sets up sgx device plugin and related components on minikube
 
 # daemonset to advertise EPC capacity to the API server
-kubectl delete -k https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/sgx_plugin/overlays/epc-register/?ref=v0.27.1
+kubectl delete -k https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/sgx_plugin/overlays/epc-register/?ref=v0.32.1
 
 # sgx admission webhook
-kubectl delete -k https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/sgx_admissionwebhook/overlays/default-with-certmanager/?ref=v0.27.1
+kubectl delete -k https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/sgx_admissionwebhook/overlays/default-with-certmanager/?ref=v0.32.1
