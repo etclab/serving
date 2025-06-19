2f199a49958de7c8d093cbcacef137b6fd5e4e4b
Aashutosh Poudel <apoudel01@wm.edu>
2025-06-19 17:04:53 -0400

    update config version to v0.27.1 doesn't work for latest version

--- dev/sgx/deploy-sgx-plugin.sh
+++ dev/sgx/deploy-sgx-plugin.sh
@@ -2,8 +2,9 @@
 
 # sets up sgx device plugin and related components on minikube
 
+# why only v0.27.1? works
 # daemonset to advertise EPC capacity to the API server
-kubectl apply -k https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/sgx_plugin/overlays/epc-register/?ref=v0.32.1
+kubectl apply -k https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/sgx_plugin/overlays/epc-register/?ref=v0.27.1
 
 # sgx admission webhook
-kubectl apply -k https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/sgx_admissionwebhook/overlays/default-with-certmanager/?ref=v0.32.1
+kubectl apply -k https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/sgx_admissionwebhook/overlays/default-with-certmanager/?ref=v0.27.1
