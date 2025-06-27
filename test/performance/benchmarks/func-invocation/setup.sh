28464674cdc71e9d5f6d912577d50d1f344002ff
Aashutosh Poudel <apoudel01@wm.edu>
2025-06-27 12:46:20 -0400

    use existing benchmark framework for measuring function invocation  - can run for a duration with set throughput/rate  - reports min,max,p50,p95,p99, total bytes and errors
    currently fixes activator in the path and limits the number of function
    replicas to 1
     - this means request goes through the activator before reaching
       function's queue-proxy

---
+++ test/performance/benchmarks/func-invocation/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${SCRIPT_DIR}/../../../../eval/s/env.sh"
+
+ns=default
+
+ko delete -f ${SCRIPT_DIR}/func-invocation-setup.yaml
+ko apply --sbom=none -Bf ${SCRIPT_DIR}/func-invocation-setup.yaml
+
+kubectl wait --timeout=60s --for=condition=ready ksvc -n "$ns" --all
+kubectl wait --timeout=60s --for=condition=available deploy -n "$ns" deployment
