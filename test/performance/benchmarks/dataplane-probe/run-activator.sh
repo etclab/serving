74d9c9c7dd828c5dccbad4bd14e2ac7bee4dd5e4
Aashutosh Poudel <apoudel01@wm.edu>
2025-06-27 12:50:36 -0400

    helper scripts to make existing knative serving benchmark work

---
+++ test/performance/benchmarks/dataplane-probe/run-activator.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${SCRIPT_DIR}/../../../../eval/s/env.sh"
+
+ns=default
+
+function run_job() {
+  local name=$1
+  local file=$2
+
+  # cleanup from old runs
+  kubectl delete job "$name" -n "$ns" --ignore-not-found=true
+
+  # start the load test and get the logs
+  envsubst < "$file" | ko apply --sbom=none -Bf -
+
+  # sleep a bit to make sure the job is created
+  sleep 5
+
+  # Follow logs to wait for job termination
+  kubectl wait --for=condition=ready -n "$ns" pod --selector=job-name="$name" --timeout=-1s
+  kubectl logs -n "$ns" -f "job.batch/$name"
+
+  # Dump logs to a file to upload it as CI job artifact
+  kubectl logs -n "$ns" "job.batch/$name" >"$ARTIFACTS/$name.log"
+
+  # clean up
+  kubectl delete "job/$name" -n "$ns" --ignore-not-found=true
+  kubectl wait --for=delete "job/$name" --timeout=60s -n "$ns"
+}
+
+run_job dataplane-probe-activator "${SCRIPT_DIR}/dataplane-probe-activator.yaml"
