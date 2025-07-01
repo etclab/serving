aa793aa867409b30740f5a746b863629725fa788
Aashutosh Poudel <apoudel01@wm.edu>
2025-06-30 22:49:10 -0400

    add script to repeatedly measure duration for pod deployment  - difference is calculated as (`ContainerReady` timestamp - `PodScheduled` timestamp)    apparently `kube-state-metrics`'s timestamp resolution is limited to seconds
    transform.py - calculates the duration and find the min,max,mean,std
    
    run-func-deploy.sh - runs the benchmark
     - each run deploys a new functions and uses `kube-state-metrics` api to measure
       the kube_pod_status_scheduled_time and kube_pod_status_ready_time

---
+++ test/performance/benchmarks/func-deployment/transform.py
@@ -0,0 +1,52 @@
+#!/bin/python
+
+import argparse
+import csv
+import statistics as stat
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "data_file", type=str, help="path to data file"
+    )
+    
+    parser.add_argument(
+        "out_file", type=str, help="path to out file"
+    )
+    
+    args = parser.parse_args()
+
+    # .data file but has actually csv
+    data_file = args.data_file
+    with open(data_file, mode="r", newline='') as file:
+        reader = csv.DictReader(file)
+        
+        all = []
+        for row in reader:
+            
+            ready_time = row['pod_ready_time']
+            scheduled_time = row['pod_scheduled_time']
+            
+            diff = int(ready_time) - int(scheduled_time)
+            
+            all.append(diff)
+        
+        all.sort()
+        # ignore the first and last five
+        tall = all[5:-5]
+
+        min_val = min(tall)
+        max_val = max(tall)
+        mean = stat.mean(tall)
+        std = stat.stdev(tall)
+        
+        print(f"min: {min_val}, max: {max_val}, mean: {mean}, std: {std}")
+        
+        with open(args.out_file, "w") as f:
+            f.write(f"# pod deployment time (seconds)\n")
+            f.write(f"{'# function name':<25} {'min':<25} {'max':<25} {'mean':<25} {'std':<25}\n")
+            f.write(f"{'autoscale':<25} {min_val:<25} {max_val:<25} {mean:<25} {std:<25}\n")
+
+if __name__ == "__main__":
+    main()
