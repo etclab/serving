d226a699bad77724e0612cce6ee869f15080a15b
Aashutosh Poudel <apoudel01@wm.edu>
2026-04-29 20:16:00 -0400

    install screen on prepare

--- artifacts/prepare.sh
+++ artifacts/prepare.sh
@@ -48,6 +48,7 @@ install_git()     { install_apt_pkg git; }
 install_gnuplot() { install_apt_pkg gnuplot; }
 install_cpuid()   { install_apt_pkg cpuid; }
 install_pip()     { install_apt_pkg python3-pip; }
+install_screen()  { install_apt_pkg screen; }
 
 have_py_pkg() {
     python3 -c "import $1" >/dev/null 2>&1
@@ -191,6 +192,7 @@ check_or_install ko       "ko version"                  install_ko
 check_or_install az       "az version"                  install_az
 log "NOTE: If you plan to use a remote (e.g. AKS) cluster, run 'az login' to authenticate the Azure CLI before running the benchmark setup scripts."
 check_or_install cpuid    "cpuid -v"                    install_cpuid
+check_or_install screen   "screen --version"            install_screen
 
 # Python packages required by benchmark scripts (e.g. parse-traces.py).
 ensure_py_pkg requests
